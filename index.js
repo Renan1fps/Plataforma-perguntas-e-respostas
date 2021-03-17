2ce4322662cbe4eb235d780eb37618d932d9be09
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-17 16:24:02 -0300

    Estruturas condicionais com EJS

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ app.set("view engine", "ejs");
 app.get("/:nome/:lang", (req, res) => {
   var nome = req.params.nome;
   var lang = req.params.lang;
+  var msg= true;
   res.render("index.ejs", {
     nome: nome,
     lang: lang,
+    mensagem: msg
   });
 });
 
