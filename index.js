f6ffc3fbdf8b8216fd48dc92d3520a2d80ee2bd8
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-17 18:36:44 -0300

    Arquivos estaticos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static('public'))
 
 app.get("/:nome/:lang", (req, res) => {
   var nome = req.params.nome;
