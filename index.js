5b446e62712176ec6685fe66394c53d994e198a7
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-19 14:35:54 -0300

    Envio de formulário para o node

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/perguntar", (req,res)=>{
-    res.render("perguntar.ejs")
-})
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar.ejs");
+});
 
-
-
-
-
-
-
-
-
+app.post("/salvarpergunta", (req, res) => {
+  res.send("formulario recebido!");
+});
 
 app.listen(8080, (erro) => {
   if (erro) {
