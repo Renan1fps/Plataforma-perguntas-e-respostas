3108d021b0802de2d2656dbd9447d836db2d7787
Renan Oliveira <renan.sergipe68@gmail.com>
2021-03-19 15:52:12 -0300

    Gerando tabela com model

---
+++ dataBase/Pergunta.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("sequelize");
+const connection = require("./dataBase");
+
+const Pergunta = connection.define("perguntas", {
+  titulo: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  descricao: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
+
+Pergunta.sync({ force: false }).then(() => {});
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
-const connection=require("./dataBase/dataBase")
+const connection = require("./dataBase/dataBase");
+const perguntaModel = require("./dataBase/Pergunta");
 
-connection.authenticate().then(()=>{
-  console.log("Conexão estabelecida com banco de dados!")
-})
-.catch((erro)=>{
-  console.log("erro")
-})
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão estabelecida com o banco de dados!");
+  })
+  .catch((erro) => {
+    console.log("erro");
+  });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,7 +28,9 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta", (req, res) => {
   const titulo = req.body.titulo;
   const descricao = req.body.descricao;
-  res.send("formulario recebido! "+ `Titulo: ${titulo}, Descrição: ${descricao}`);
+  res.send(
+    "formulario recebido! " + `Titulo: ${titulo}, Descrição: ${descricao}`
+  );
 });
 
 app.listen(8080, (erro) => {
